e8086a8b17ec5812bf072f83b5797e96d36d6703
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-07-14 16:55:10 +0200

    Added "set"

--- run.py
+++ run.py
@@ -3,39 +3,40 @@ from cgi import print_arguments
 from glob import glob
 from multiprocessing.connection import wait
 from operator import contains
-import os 
+import os
 import time
 import getpass
 import json
+import os.path
+from os import path
 
 
-#Variables:
 
-#Declares if the console should repeat itself
-global activate 
+# Variables:
+
+# Declares if the console should repeat itself
+global activate
 activate = False
 
-#goes before user input
+# goes before user input
 nfname = "nf1 >>> "
 
-#contains chosen input
+# contains chosen input
 chose = ""
 
-#determines if the user had an exploit before 
+# determines if the user had an exploit before
 hadexploit = False
 
-#determines what exploit is active currently
+# determines what exploit is active currently
 currentexploit = "none"
 
-#side variable for the run command
-runcommand = False
 
 exploitcollections = """Exploit collections:
 
         Nuke - Basic Nuke framework tools
         Nukeware - Malware collection of the Nuke Framework"""
 
-#confirm if a command was activated
+# confirm if a command was activated
 confirmcommand = False
 
 currentdir = os.getcwd()
@@ -54,18 +55,21 @@ nukecontents = """
 User = getpass.getuser()
 
 
-
-#Functions:
+# Functions:
 
 # define a clear command
 
 # going to current dir
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
 def clr():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
+
+
 def animation():
     animation = ["="]
     for i in range(66):
@@ -79,22 +83,20 @@ def animation():
     time.sleep(0.3)
 
 
-def askrun():
-    global chose
-    runcommand = True
-    chose = input("Type the Name of your wanted exploit here: ")
-
 def startinputter():
     global activate
     activate = True
+
 
 def stopinputter():
     global activate
     activate = False
 
+
 def confirm(x):
     global confirmcommand
     confirmcommand = x
+
 
 def nowrunning(exploit, file):
     clr()
@@ -104,6 +106,7 @@ def nowrunning(exploit, file):
     os.system("python3 " + file)
     exit()
 
+
 def getlocaluser():
     try:
         global User
@@ -112,28 +115,59 @@ def getlocaluser():
         User = "(Could not get local user)"
     return User
 
+
 def printuser():
     if namedata["standard"] == True:
         return getlocaluser()
     else:
         return namedata["name"]
 
+
 def openjson(file):
-    
-    os.chdir (currentdir + "/Saves")
+
+    os.chdir(currentdir + "/Saves")
     with open(file) as f:
         data = json.load(f)
-    
+
     return data
-    
-    
-
-    
 
 
-#Variables which require declared methods:
+def restart():
+    print("Restarting...")
+    time.sleep(1)
+    settingdata = openjson("settings.json")
+    if settingdata["animation"] == True:
+        animation = ["="]
+        for i in range(66):
+            print("".join(animation))
+            print("".join(animation))
+            animation.append("═")
+            time.sleep(0.015)
+            clr()
+        print("═══════════════════════════════════════════════════════════════════")
+        print("═══════════════════════════════════════════════════════════════════")
+        time.sleep(0.3)
+    
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    namedata = openjson("Name.json")
+    print("Restarted Nuke Framework! logged in as " +
+          printuser() + " (Type help to get Options)\n")
+    time.sleep(1)
+    startinputter()
+    confirm(True)
+
+
+
+
+# Variables which require declared methods:
 namedata = openjson("Name.json")
 isnewdata = openjson("New.json")
+
+
+
 
 if isnewdata["isnew"] == True:
     clr()
@@ -142,105 +176,113 @@ if isnewdata["isnew"] == True:
     changeisnewdata = {"isnew": False}
     with open("New.json", "w") as f:
         json.dump(changeisnewdata, f)
-    
 
+settingdata = openjson("Settings.json")
+if settingdata["animation"] == True:
+    animation()
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
 
+    time.sleep(0.1)
+    print("W")
+    time.sleep(0.015)
+    clr()
 
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("We")
+    time.sleep(0.015)
+    clr()
 
-animation()
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Wel")
+    time.sleep(0.015)
+    clr()
 
-time.sleep(0.1)
-print("W")
-time.sleep(0.015)
-clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welc")
+    time.sleep(0.015)
+    clr()
 
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("We")
-time.sleep(0.015)
-clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welco")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcom")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcome")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcome b")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcome ba")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcome bac")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcome back")
+    time.sleep(0.015)
+    clr()
+    print("═══════════════════════════════════════════════════════════════════")
+    os.system("figlet -f smslant Nuke Framework")
+    print("═══════════════════════════════════════════════════════════════════")
+    print("Welcome back!")
+    time.sleep(1)
 
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Wel")
-time.sleep(0.015)
-clr()
-
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welc")
-time.sleep(0.015)
-clr()
-
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welco")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcom")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcome")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcome b")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcome ba")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcome bac")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcome back")
-time.sleep(0.015)
-clr()
-print("═══════════════════════════════════════════════════════════════════")
-os.system("figlet -f smslant Nuke Framework")
-print("═══════════════════════════════════════════════════════════════════")
-print("Welcome back!")
-time.sleep(1)
 clr()
 print("═══════════════════════════════════════════════════════════════════")
 os.system("figlet -f smslant Nuke Framework")
 print("═══════════════════════════════════════════════════════════════════")
 
-print("NukeFramework is ready! You are logged in as " + printuser() + " (Type help to get Options)\n")
+print("NukeFramework is ready! You are logged in as " +
+      printuser() + " (Type help to get Options)\n")
 
 startinputter()
 while activate == True:
+    def cleanspace(x):
+        while x.startswith(" ") == True:
+            x = x.removeprefix(" ")
+        global inputter
+        return x
     confirm(False)
     inputter = input(nfname)
+    inputtercopy = inputter
+    
+        
     if inputter.startswith(" "):
         while inputter.startswith(" "):
             inputter = inputter.removeprefix(" ")
-    #basics
+    # basics
     if inputter.startswith("help") == True:
         print("""
         Basics:
@@ -251,10 +293,18 @@ while activate == True:
         restart - restarts the program
         ce *ARGUMENT* - change Exploit collection (if left blank will exit current Exploit collection)
         run *ARGUMENT* - run an exploit (if in collection)
+        reset - reset all settings to default
         
         Misc:
-        name *New Name* - changes your name that gets displayed when you open nf1
-        name *-r* - changes your name to the default login name of your Computer
+        name *ARGUMENT* - allows you to change your name
+        Arguments for name:
+            name *New Name* - changes your name that gets displayed when you open nf1
+            name *-r* - changes your name to the default login name of your Computer
+
+        set *ARGUMENT* true/false - sets a setting
+            Arguments for set:
+            set animation - allows you to (de)activate the animation at the start of the program
+        
         """ + exploitcollections)
         confirm(True)
     if inputter.startswith("exit") == True:
@@ -266,21 +316,9 @@ while activate == True:
         confirm(True)
     if inputter.startswith("restart") == True:
         stopinputter()
-        
-        print("Restarting...")
-        time.sleep(1)
-        animation()
-        clr()
-        print("═══════════════════════════════════════════════════════════════════")
-        os.system("figlet -f smslant Nuke Framework")
-        print("═══════════════════════════════════════════════════════════════════")
-        namedata = openjson("Name.json")
-        print("Restarted Nuke Framework! logged in as " + printuser() + " (Type help to get Options)\n")
-        time.sleep(1)
-        startinputter()
-        confirm(True)
-    
-    #exploit collection changers
+
+        restart()
+    # exploit collection changers
     if inputter.startswith("ce") == True:
         inputter = inputter.removeprefix("ce ")
         inputter = inputter.replace(" ", "")
@@ -302,9 +340,8 @@ while activate == True:
         hadexploit = True
         currentexploit = "Nuke"
         confirm(True)
-   
 
-    #ls commands
+    # ls commands
     if inputter.startswith("ls") == True:
         inputter = inputter.removeprefix("ls")
         inputter = inputter.replace(" ", "")
@@ -363,25 +400,62 @@ while activate == True:
             json.dump(newnamedata, f)
         print("Name has been resetted to your login name")
         confirm(True)
+    if inputter.startswith("reset") == True:
+        chose = input(
+            "Are you sure you want to reset all your Settings? (y/n): ")
+        if "y" in chose:
+            newnamedata = {
+                "standard": True,
+                "none": newname,
+            }
+            newdata = {
+                "isnew": True,
+            }
+            os.chdir("../Saves")
+            with open("Name.json", "w") as f:
+                json.dump(newnamedata, f)
+            with open("New.json" , "w") as f:
+                json.dump(newdata, f)
+            confirm(True)
+            exit()
+            
+        if inputter.startswith("n") == True:
+            print("Settings have not been resetted")
+            confirm(True)
+    if inputter.startswith("set") == True:
+        confirm(True)
+        inputter = cleanspace(inputter)
+        if inputter == "set":
+            print("You need to specify a setting")
+            confirm(True)
+        inputter = inputter.removeprefix("set")
+        inputter = cleanspace(inputter)
+        if inputter.startswith("animation") == True:
+            inputter = inputter.removeprefix("animation")
+            inputter = inputter.replace(" ", "")
+            if inputter == "":
+                print("You need an true or false argument")
+                confirm(True)
+            if inputter == "true":
+                animation = True
+                print("Starting Animation is now enabled")
+                confirm(True)
+                settingdata = openjson("Settings.json")
+                settingdata["animation"] = True
+                with open("Settings.json", "w") as f:
+                    json.dump(settingdata, f)
+                
+            if inputter == "false":
+                animation = False
+                print("Starting Animation is now disabled")
+                confirm(True)
+                settingdata = openjson("Settings.json")
+                settingdata["animation"] = False
+                with open("Settings.json", "w") as f:
+                    json.dump(settingdata, f)
+                
 
-
-
-
+            
 
     if confirmcommand == False:
         print(inputter + " is no valid command!")
-
-
-    
-        
-            
-
-    
-    
-    
-    
-
-
-
-
-
